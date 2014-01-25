ca7cd023b6388368c6be4b4ee8c33b037529426d
Eric Mignot <eric.mignot@gmail.com>
2014-01-25 11:18:51 -0500

    select correct case

--- world.2/lib/extract.parameter.js
+++ world.2/lib/extract.parameter.js
@@ -1,7 +1,24 @@
-var url = require('url');
+var url       = require('url');
 var theString = require('./string.is.a.number');
-var answersWithErrorNotANumber = require('./answers.with.not.a.number');
-var answersWithDecomposition = require('./answers.with.decomposition');
+
+var cases = [
+    {
+        isMatching: function(parameter) {
+            return theString(parameter).isANumber();
+        },
+        answer: function(parameter) {
+            return require('./answers.with.decomposition')(parameter);
+        }
+    },
+    {
+        isMatching: function(parameter) {
+            return !theString(parameter).isANumber();
+        },
+        answer: function(parameter) {
+            return require('./answers.with.not.a.number')(parameter);
+        }
+    },
+];
 
 var extractParameter = function(name) {
 
@@ -9,31 +26,11 @@ var extractParameter = function(name) {
         
         fromQueryStringIn: function(request) {
         	var parameter = url.parse(request.url, true).query[name];
-
-            var cases = [
-                {
-                    isMatching: function(parameter) {
-                        return theString(parameter).isANumber();
-                    },
-                    answer: function(parameter) {
-                        return answersWithDecomposition(parameter);
-                    }
-                },
-                {
-                    isMatching: function(parameter) {
-                        return !theString(parameter).isANumber();
-                    },
-                    answer: function(parameter) {
-                        return answersWithErrorNotANumber(parameter);
-                    }
-                },
-            ];
-            if (cases[0].isMatching(parameter)) {
-                return cases[0].answer(parameter);
-            }
             
-            if (cases[1].isMatching(parameter)) {
-                return cases[1].answer(parameter);
+            for (var i = 0; i<cases.length; i++) {
+                if (cases[i].isMatching(parameter)) {
+                    return cases[i].answer(parameter);
+                }
             }
         }
     }    
