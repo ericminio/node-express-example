e77bc321a9dea437bc6226b5c28ed7be612ac18f
Eric Mignot <eric.mignot@gmail.com>
2014-01-25 01:41:24 -0500

    passes contact information challenge

---
+++ world.1/1.3.contact.spec.js
@@ -0,0 +1,34 @@
+var gotoRoot = '../';
+var describeYoseLevel   = require(gotoRoot + 'describe.yose.level');
+var request             = require('request');
+var cheerio             = require('cheerio');
+
+describeYoseLevel('Passing the contact information level:', function() {
+
+    describe('home page', function() {
+       
+       var page;
+       
+       beforeEach(function(done) {
+           request('http://localhost:7000/', function(error, response, body) {
+               page = cheerio.load(body);
+               done();
+           });
+       });
+       
+       it('has a a#contact-me-link element', function() {
+           expect(page).toHaveElement('a#contact-me-link');
+       });
+       
+       describe('contact-me element', function() {
+           
+           it('targets my LinkedIn account', function() {
+               expect(page('a#contact-me-link').attr('href')).toEqual('http://ca.linkedin.com/in/ericmignot');
+           });  
+           
+           it('has a text', function() {
+               expect(page('a#contact-me-link').text().length).toBeGreaterThan(0);
+           });       
+       });
+    });   
+});
---
+++ world.1/1.4.share.spec.js
@@ -0,0 +1,43 @@
+var gotoRoot = '../';
+var describeYoseLevel   = require(gotoRoot + 'describe.yose.level');
+var request             = require('request');
+var cheerio             = require('cheerio');
+var fs                  = require('fs');
+
+describeYoseLevel('Passing the share level:', function() {
+
+    describe('Repository link', function() {
+       
+       var page;
+       
+       beforeEach(function(done) {
+           request('http://localhost:7000/', function(error, response, body) {
+               page = cheerio.load(body);
+               done();
+           });
+       });
+       
+       it('exists', function() {
+           expect(page).toHaveElement('a#repository-link');
+       });
+
+       it('references my github repo', function() {
+           expect(page('a#repository-link').attr('href')).toEqual('https://github.com/ericminio/yose-node-express');
+       });
+    });
+    
+    describe('repo README', function() {
+
+        var readme;
+
+        beforeEach(function() {
+            readme = fs.readFileSync('./README.md').toString();
+        });
+        
+        it('contains reference to the game', function() {
+            expect(readme).toContain('YoseTheGame');
+        });
+        
+    });
+   
+});
